311e3496ba5d13d73c503d5511e7bbae2b57eb59
itemNo143 <kzitemno143@gmail.com>
2020-07-28 21:04:54 +0900

    migrateファイルの修正

--- db/migrate/20200724110024_create_items.rb
+++ db/migrate/20200724110024_create_items.rb
@@ -5,10 +5,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :price, null: false
       t.text :description, null: false
       t.string :stock, null: false
-      t.references :condition, null: false, foreign_key: true
-      t.references :shipping_cost, null: false, foreign_key: true
-      t.references :shipping_time, null: false, foreign_key: true
-      t.references :prefecture, null: false, foreign_key: true
+      t.integer :condition_id, null: false
+      t.integer :shipping_cost_id, null: false
+      t.integer :shipping_time_id, null: false
+      t.integer :prefecture_id, null: false
       t.references :category, null: false, foreign_key: true
       t.references :brand, foreign_key: true
       t.references :seller, null: false, foreign_key: true
