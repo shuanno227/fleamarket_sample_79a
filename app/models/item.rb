58d3bfce23483eff1dd9d19064f7980cdf051ff9
YourName <you@example.com>
2020-07-30 10:37:12 +0900

    Create new items index

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,4 +25,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   delegate :name, to: :prefecture
 
+  enum status: { sell: 0, buy: 1 , trading:2}
+  scope :on_sell, -> { where(status: 0) }
 end
