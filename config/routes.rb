cea66502e08ab4eda1bd5d4b6eee939b61f9cb53
itemNo143 <kzitemno143@gmail.com>
2020-08-03 04:06:30 +0900

    クレジットカード購入(決済)機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root to: "items#index"
+  root to: 'items#index'
   resources :items
   resources :users
-  resources :credit_cards, only: [:index, :new, :create, :destroy] do
+  resources :credit_cards, only: [:index, :new, :create, :destroy, :buy] do
+    get 'buy', on: :member
   end
 end
