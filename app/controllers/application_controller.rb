83ea9449c46a3718a507b312811448a8f6c659a0
minami0601 <minami.love.bb@icloud.com>
2020-08-03 16:52:11 +0900

    ヘッダーカテゴリー一覧の見た目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+  prepend_before_action :category
 
   private
   
@@ -23,4 +24,9 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def category
+    @parents = Category.where(ancestry: nil)
+  end
+
 end
