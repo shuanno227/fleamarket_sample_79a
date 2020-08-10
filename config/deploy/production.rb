ae6449e6220fc10f1d6edbf692278bd023fe7a48
shuanno227 <shuanno227@gmail.com>
2020-08-10 13:40:29 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.199.19.138', user: 'ec2-user', roles: %w{app db web}
+server '54.150.205.39', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, "production"
 set :unicorn_rack_env, "production"
