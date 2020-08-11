e4333f524cf88fd9be8bfd66c551f9139d268843
itemNo143 <kzitemno143@gmail.com>
2020-08-11 16:12:53 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,15 @@
 class ItemsController < ApplicationController
   before_action :index_category_set, only: :index
   before_action :set_item, only: [:show, :edit, :update, :destroy, :confirm]
-  before_action :show_all_instance, only: [:show, :edit,:update, :destroy]
+  before_action :show_all_instance, only: [:show, :edit, :update, :destroy]
   before_action :set_card, only: [:confirm]
   before_action :login, except: [:index, :show]
 
   def index
-    @items = Item.includes([:images]).order(created_at: :desc)
-    @ladies = Item.includes(:images).where(category_id: 33..211).order("id DESC")
-    @mens = Item.includes(:images).where(category_id: 226..356).order("id DESC")
-    @babies = Item.includes(:images).where(category_id: 372..490).order("id DESC")
+    @items    = Item.includes([:images]).order(created_at: :desc)
+    @ladies   = Item.includes(:images).where(category_id: 33..211).order("id DESC")
+    @mens     = Item.includes(:images).where(category_id: 226..356).order("id DESC")
+    @babies   = Item.includes(:images).where(category_id: 372..490).order("id DESC")
     @interior = Item.includes(:images).where(category_id: 540..633).order("id DESC")
   end
 
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
     if user_signed_in?
       @item = Item.new
       @item.images.new
-      
+
     else
       redirect_to new_user_session_path
     end
@@ -37,32 +37,32 @@ class ItemsController < ApplicationController
     unless current_user.id == @item.seller_id
       redirect_to '/'
     end
-    @image = Image.where(item_id: @item)
-    gon.imageLength = @image.length
+    @image                 = Image.where(item_id: @item)
+    gon.imageLength        = @image.length
     @category_parent_array = Category.where(ancestry: nil)
-    grandchild = @item.category
-    child = grandchild.parent
-  
+    grandchild             = @item.category
+    child                  = grandchild.parent
+
     @parent_array = []
     @parent_array << @item.category.parent.parent.name
     @parent_array << @item.category.parent.parent.id
-  
+
     @category_children_array = Category.where(ancestry: child.ancestry)
-    @child_array = []
+    @child_array             = []
     @child_array << child.name
     @child_array << child.id
 
     @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry)
-    @grandchild_array = []
+    @grandchild_array             = []
     @grandchild_array << grandchild.name
     @grandchild_array << grandchild.id
   end
 
   def update
-    grandchild = @item.category
-    child = grandchild.parent
-    @category_parent_array = Category.where(ancestry: nil)
-    @category_children_array = Category.where(ancestry: child.ancestry)
+    grandchild                    = @item.category
+    child                         = grandchild.parent
+    @category_parent_array        = Category.where(ancestry: nil)
+    @category_children_array      = Category.where(ancestry: child.ancestry)
     @category_grandchildren_array = Category.where(ancestry: grandchild.ancestry)
     if item_params[:images_attributes].nil?
       flash.now[:alert] = "更新できませんでした 【画像を１枚以上入れてください】"
@@ -72,12 +72,12 @@ class ItemsController < ApplicationController
       item_params[:images_attributes].each do |a, b|
         exit_ids << item_params[:images_attributes].dig(:"#{a}", :id).to_i
       end
-      ids = Image.where(item_id: params[:id]).map { |image| image.id }
+      ids        = Image.where(item_id: params[:id]).map { |image| image.id }
       delete__db = ids - exit_ids
       Image.where(id: delete__db).destroy_all
       @item.touch
       if @item.update(item_params)
-        redirect_to  item_path
+        redirect_to item_path
       else
         flash.now[:alert] = '更新できませんでした'
         render :edit
@@ -87,16 +87,16 @@ class ItemsController < ApplicationController
 
 
   def show
-    @image = Image.where(item_id: @item)
+    @image             = Image.where(item_id: @item)
     @item_grandchildId = Category.find(@item.category_id)
-    @item_childId = @item_grandchildId.parent
-    @item_parentId = Category.find(@item_childId.ancestry)
-    @condition = Condition.find(@item.condition_id)
-    @shipping_cost = ShippingCost.find(@item.shipping_cost_id)
-    @shipping_time = ShippingTime.find(@item.shipping_time_id)
-    @prefecture = Prefecture.find(@item.prefecture_id)
-    @price = @item.price
-    gon.imageId = @image.ids
+    @item_childId      = @item_grandchildId.parent
+    @item_parentId     = Category.find(@item_childId.ancestry)
+    @condition         = Condition.find(@item.condition_id)
+    @shipping_cost     = ShippingCost.find(@item.shipping_cost_id)
+    @shipping_time     = ShippingTime.find(@item.shipping_time_id)
+    @prefecture        = Prefecture.find(@item.prefecture_id)
+    @price             = @item.price
+    gon.imageId        = @image.ids
   end
 
   def destroy
@@ -143,12 +143,12 @@ class ItemsController < ApplicationController
       # find_itemメソッドで処理
       find_item(category)
 
-    # 孫カテゴリーを選択していた場合の処理
+      # 孫カテゴリーを選択していた場合の処理
     elsif @category.ancestry.include?("/")
       # Categoryモデル内の親カテゴリーに紐づく孫カテゴリーのidを取得
       @items = Item.where(category_id: params[:id])
 
-    # 子カテゴリーを選択していた場合の処理
+      # 子カテゴリーを選択していた場合の処理
     else
       category = Category.find_by(id: params[:id]).child_ids
       # 孫カテゴリーに該当するitemsテーブルのレコードを入れるようの配列を用意
@@ -159,7 +159,7 @@ class ItemsController < ApplicationController
   end
 
   def confirm
-    @image = @item.images
+    @image    = @item.images
     @adresses = Address.find(current_user.id)
     # 売り切れ商品に直接パス指定され購入されそうになった時用
     if @item.buyer_id.present?
@@ -182,19 +182,19 @@ class ItemsController < ApplicationController
 
       # クレジットカード会社を取得したので、カード会社の画像をviewに表示させるため、ファイルを指定する。
       case @card_brand
-      when 'Visa'
-        # 例えば、Pay.jpからとってきたカード情報の、ブランドが"Visa"だった場合は返り値として(画像として登録されている)Visa.pngを返す
-        @card_image = 'visa.gif'
-      when 'JCB'
-        @card_image = 'jcb.gif'
-      when 'MasterCard'
-        @card_image = 'master.png'
-      when 'American Express'
-        @card_image = 'amex.gif'
-      when 'Diners Club'
-        @card_image = 'diners.gif'
-      when 'Discover'
-        @card_image = 'discover.gif'
+        when 'Visa'
+          # 例えば、Pay.jpからとってきたカード情報の、ブランドが"Visa"だった場合は返り値として(画像として登録されている)Visa.pngを返す
+          @card_image = 'visa.gif'
+        when 'JCB'
+          @card_image = 'jcb.gif'
+        when 'MasterCard'
+          @card_image = 'master.png'
+        when 'American Express'
+          @card_image = 'amex.gif'
+        when 'Diners Club'
+          @card_image = 'diners.gif'
+        when 'Discover'
+          @card_image = 'discover.gif'
       end
 
       #  viewの記述を簡略化
@@ -209,20 +209,17 @@ class ItemsController < ApplicationController
 
   def login
     unless user_signed_in?
-      redirect_to new_user_session_path 
+      redirect_to new_user_session_path
     end
   end
 
   def item_params
-    params.require(:item).permit(:image_ids, :name, :price, :description, :condition_id, :shipping_cost_id, :shipping_time_id, :prefecture_id, :category_id, :brand, :buyer_id, :seller_id, images_attributes: [:image,:_destroy, :id]).merge(seller_id: current_user.id, category_id: params[:category_id])
+    params.require(:item).permit(:image_ids, :name, :price, :description, :condition_id, :shipping_cost_id, :shipping_time_id, :prefecture_id, :category_id, :brand, :buyer_id, :seller_id, images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id, category_id: params[:category_id])
   end
 
-  
   def set_item
     @item = Item.find(params[:id])
   end
-
-  
 
   def show_all_instance
     @user                = User.find(@item.seller_id)
@@ -249,12 +246,11 @@ class ItemsController < ApplicationController
     end
   end
 
-
   def index_category_set
     array = [1, 2, 3, 4]
     for num in array do
       search_anc = Category.where('ancestry LIKE(?)', "#{num}/%")
-      ids = []
+      ids        = []
       search_anc.each do |i|
         ids << i[:id]
       end
